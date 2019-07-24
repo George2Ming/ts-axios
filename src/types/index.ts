1c69b33ec58177c2b96e1812a6473438d6996556
George2Ming <1017491736@qq.com>
2019-07-24 22:51:21 +0800

    添加类型判断

--- src/types/index.ts
+++ src/types/index.ts
@@ -16,8 +16,8 @@ export interface AxiosRequestConfig {
   timeout?: number // 超时时间
 }
 
-export interface AxiosResponse {
-  data: any
+export interface AxiosResponse<T=any> {
+  data: T
   status: number
   statusText: string
   headers: any
@@ -25,7 +25,7 @@ export interface AxiosResponse {
   request: any
 }
 
-export interface AxiosPromise extends Promise<AxiosResponse>{}
+export interface AxiosPromise<T=any> extends Promise<AxiosResponse<T>>{}
 
 export interface AxiosError extends Error{
   isAxiosError:boolean
@@ -36,27 +36,27 @@ export interface AxiosError extends Error{
 }
 
 export interface Axios {
-  request(config: AxiosRequestConfig): AxiosPromise
+  request<T=any>(config: AxiosRequestConfig): AxiosPromise<T>
 
-  get(url:string,config?:AxiosRequestConfig): AxiosPromise
+  get<T=any>(url:string,config?:AxiosRequestConfig): AxiosPromise<T>
 
-  delete(url:string,config?:AxiosRequestConfig): AxiosPromise
+  delete<T=any>(url:string,config?:AxiosRequestConfig): AxiosPromise<T>
 
-  head(url:string,config?:AxiosRequestConfig): AxiosPromise
+  head<T=any>(url:string,config?:AxiosRequestConfig): AxiosPromise<T>
 
-  options(url:string,config?:AxiosRequestConfig): AxiosPromise
+  options<T=any>(url:string,config?:AxiosRequestConfig): AxiosPromise<T>
 
-  post(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise
+  post<T=any>(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise<T>
 
-  put(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise
+  put<T=any>(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise<T>
 
-  patch(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise
+  patch<T=any>(url:string, data?:any, config?:AxiosRequestConfig): AxiosPromise<T>
 }
 
 // axios实例类型
 export interface AxiosInstance extends Axios{
   // 直接传一个对象
-  (config:AxiosRequestConfig): AxiosPromise
+  <T=any>(config:AxiosRequestConfig): AxiosPromise<T>
   // 第一个参数穿地址,后续config可选
-  (url:string, config?:AxiosRequestConfig): AxiosPromise
+  <T=any>(url:string, config?:AxiosRequestConfig): AxiosPromise<T>
 }
